6e004ef7e742cbf7fde5ea72f4935e5b73d6b882
benjmain roth <benjaminsavanne@gmail.com>
2020-11-17 04:50:05 -0600

    add update bird route to birds router

--- routes/BirdsRouter.js
+++ routes/BirdsRouter.js
@@ -4,5 +4,6 @@ const BirdsController = require('../controllers/BirdsController')
 Router.get('/:bird_id',  BirdsController.GetBirdById)
 Router.post('/create', BirdsController.CreateBird)
 Router.delete('/:bird_id', BirdsController.DeleteBird)
+Router.put('/:bird_id', BirdsController.UpdateBird)
 
 module.exports = Router
