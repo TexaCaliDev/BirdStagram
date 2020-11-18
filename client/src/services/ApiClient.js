a4e83155a7384ffa1de62de19501017cf817c11f
benjmain roth <benjaminsavanne@gmail.com>
2020-11-18 08:58:39 -0600

    create api client

---
+++ client/src/services/ApiClient.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+
+const ApiClient = axios.create({ baseURL: process.env.NODE_ENV === 'production'
+? `${window.location.origin}/api`
+: 'http://localhost:3004/api'})
+
+ApiClient.interceptors.request.use(
+  async (config) => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (err) => Promise.reject(err)
+)
+
+ApiClient.interceptors.request.use(
+  async (config) => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (err) => Promise.reject(err)
+)
+
+export default ApiClient
+
+process.env.NODE_ENV === 'production'
+  ? `${window.location.origin}/api`
+  : 'http://localhost:3004/api/api'
