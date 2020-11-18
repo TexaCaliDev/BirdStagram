28e175fe81e6e8e2dc30cf40b90f2f3cd0739172
benjmain roth <benjaminsavanne@gmail.com>
2020-11-18 09:01:45 -0600

    adjusted app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './styles/App.css'
+import {Route, Switch} from 'react-router-dom'
+import Home from './pages/Home'
+import Router from './components/Router'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
-    </div>
+  <div className="App">
+    <Router />
+  </div>
   );
 }
 
