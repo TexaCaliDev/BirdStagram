41ce59710e74a578a071392fccdf0ea996d1abb3
benjmain roth <benjaminsavanne@gmail.com>
2020-11-18 08:55:57 -0600

    adjust connection

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose')
 
-const connection = mongoose.connect('mongodb://localhost:27017/birdDatabase', {
+const connection = mongoose.connect( process.env.NODE_ENV === 'production'
+? process.env.DATABASE_URL
+: 'mongodb://localhost:27017/birdDatabase', {
   useNewUrlParser: true,
   useFindAndModify: true,
   useUnifiedTopology: true,
   useCreateIndex: true
 })
+
+
 
 mongoose.set('debug', true)
 
