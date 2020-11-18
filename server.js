453605f499695093a6d04c4c14a367c501a4eab0
benjmain roth <benjaminsavanne@gmail.com>
2020-11-18 08:55:10 -0600

    edit server

--- server.js
+++ server.js
@@ -6,20 +6,27 @@ const connection = require('./db/connection')
 const AppRouter = require('./routes/AppRouter')
 const PORT = process.env.PORT || 3004
 const helmet = require('helmet')
+const path = require('path')
 // Initialize Middleware
 app.use(morgan('dev'))
-app.use(helmet())
+app.use(helmet({ contentSecurityPolicy: false }))
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: true
   }))
+  app.use(express.static(path.join(__dirname, 'client', 'build')))
   // Initialize Middleware
   
-app.get('/', (req, res) => res.send({
-  msg: 'oh yea'
-}))
+// app.get('/', (req, res) => res.send({
+//   msg: 'oh yea'
+// }))
+
 app.use('/api', AppRouter)
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+)
 
 app.listen(PORT, async () => {
   try {
