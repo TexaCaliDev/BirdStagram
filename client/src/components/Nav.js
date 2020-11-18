669958f80b7882eecfcf4b222229c5d62dcb5f0e
benjmain roth <benjaminsavanne@gmail.com>
2020-11-18 09:00:58 -0600

    create nav bar

---
+++ client/src/components/Nav.js
@@ -0,0 +1,32 @@
+import React , {Component} from 'react'
+import {NavLink} from 'react-router-dom'
+
+class Nav extends Component {
+    constructor(){
+        super()
+
+    }
+
+
+    render(){
+        return(
+        <nav>
+            <NavLink to="/users/register">
+                Sign Up
+            </NavLink>
+            <NavLink to="/users/login">
+                Sign In
+            </NavLink>
+            <NavLink to="/birds">
+                Discover
+            </NavLink>
+            <NavLink to="/">
+                Home
+            </NavLink>
+        </nav>
+           
+        )
+    }
+}
+
+export default Nav
